09527772f21d7ee198efa22b87c1f766a14540f4
minoupower554 <minoupower554@gmail.com>
2026-01-09 00:53:28 +0100

    feat: unified reactorTank to ChemicalStack

---
+++ mekanism/chemical_stack.lua
@@ -0,0 +1,5 @@
+---@meta
+
+---@class ChemicalStack Amount and type of a fluid
+---@field amount number The amount of the fluid in the tank
+---@field name string The type of fluid in the tank
--- mekanism/init.lua
+++ mekanism/init.lua
@@ -1,2 +1,3 @@
 ---@meta
 require('mekanism.fission_reactor')
+require('mekanism.chemical_stack')
