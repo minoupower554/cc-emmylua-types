b8913293c82c23153a7805cca2847a71ed521ed4
minoupower554 <minoupower554@gmail.com>
2026-01-09 01:20:16 +0100

    feat: added industrial turbine getMaxProduction

--- mekanism/industrial_turbine.lua
+++ mekanism/industrial_turbine.lua
@@ -5,3 +5,4 @@
 ---@field getMaxEnergy fun(): number The total amount of energy that fits in the buffer
 ---@field getSteam fun(): ChemicalStack The contents of the steam tank
 ---@field getSteamFilledPercentage fun(): number The amount of steam in the tank in percent
+---@field getMaxProduction fun(): number The maximum amount of energy the turbine can produce
