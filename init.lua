8befef5217f395c4074ac33be438ca39a6c6a029
minoupower554 <minoupower554@gmail.com>
2026-01-09 13:20:46 +0100

    feat: add path reset

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
+local old_path = package.path
 local info = debug.getinfo(1, "S")
 local path = info.source:sub(2)
 package.path = path:match("^(.*)/[^/]*$") .. "/?.lua;" .. package.path
 
 require('types.computercraft')
 require('types.mekanism')
+
+package.path = old_path -- reset path
