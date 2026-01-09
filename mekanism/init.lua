ac7cd7b83681b38318adc7d3472dcd59399b1400
minoupower554 <minoupower554@gmail.com>
2026-01-09 02:51:23 +0100

    feat: added resistive heater

--- mekanism/init.lua
+++ mekanism/init.lua
@@ -2,3 +2,4 @@
 require('mekanism.fission_reactor')
 require('mekanism.chemical_stack')
 require('mekanism.industrial_turbine')
+require('mekanism.resistive_heater')
---
+++ mekanism/resistive_heater.lua
@@ -0,0 +1,5 @@
+---@meta
+
+---@class ResistiveHeaterPeripheral : IPeripheral The mekanism resistive heater as returned by peripheral.wrap()
+---@field getEnergyUsage fun(): number Returns the set energy usage
+---@field setEnergyUsage fun(usage: number) Set the heater's energy usage
