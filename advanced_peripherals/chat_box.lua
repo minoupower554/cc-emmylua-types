047c6d01b8f4d05335117cc200bee44225e2960d
minoupower554 <minoupower554@gmail.com>
2026-01-09 22:03:49 +0100

    feat: add formatted messages

--- advanced_peripherals/chat_box.lua
+++ advanced_peripherals/chat_box.lua
@@ -3,3 +3,5 @@
 ---@class ChatBoxPeripheral : IPeripheral The chat box as returned by peripheral.wrap()
 ---@field sendMessage fun(message: string, prefix: string?, brackets: string?, bracketColor: string?, range: number?, utf8Support: boolean?): true|nil, string Sends a message to the global chat
 ---@field sendMessageToPlayer fun(message: string, username: string, prefix: string?, brackets: string?, bracketColor: string?, range: number?, utf8Support: boolean?): true|nil, string Sends a message to the specified user
+---@field sendFormattedMessage fun(message: string, prefix: string?, brackets: string?, bracketColor: string?, range: number?, utf8Support: boolean?): true|nil, string Sends a JSON formatted message to the global chat
+---@field sendFormattedMessageToPlayer fun(message: string, username: string, prefix: string?, brackets: string?, bracketColor: string?, range: number?, utf8Support: boolean?): true|nil, string Sends a JSON formatted message to the specified user
--- computercraft/textutils.lua
+++ computercraft/textutils.lua
@@ -5,3 +5,5 @@
 ---@field serialiseJSON fun(...: any, options: table?): string Return the JSON representation of the given value
 ---@field unserializeJSON fun(s: string, options: table?): any|nil, string Convert a JSON string to an object
 ---@field unserialiseJSON fun(s: string, options: table?): any|nil, string Convert a JSON string to an object
+---@type textutils
+textutils = textutils
