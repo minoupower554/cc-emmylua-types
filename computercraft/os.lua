0a53eff7ffae54e279223625e484e309e04067c8
minoupower554 <minoupower554@gmail.com>
2026-01-09 12:38:58 +0100

    feat: add queueEvent

--- computercraft/os.lua
+++ computercraft/os.lua
@@ -1,7 +1,8 @@
 ---@meta
 
 ---@class os
----@field pullEvent fun(...: string): string, any[]|nil pause execution of the current thread and wait for any events matching filter
+---@field pullEvent fun(...: string): string, any[]|nil Pause execution of the current thread and wait for any events matching filter
 ---@field startTimer fun(time: number): number Starts a timer that runs for the specified time in seconds, returns the timer id
+---@field queueEvent fun(name: string, ...: string): nil Add an event to the event queue
 ---@type os
 os = os
