d79cd093842d9fd3a71108ce43b2d47f05162be4
minoupower554 <minoupower554@gmail.com>
2026-01-09 22:01:10 +0100

    feat: add json serialize

---
+++ computercraft/textutils.lua
@@ -0,0 +1,7 @@
+---@meta
+
+---@class textutils The textutils API
+---@field serializeJSON fun(...: any, options: table?): string Return the JSON representation of the given value
+---@field serialiseJSON fun(...: any, options: table?): string Return the JSON representation of the given value
+---@field unserializeJSON fun(s: string, options: table?): any|nil, string Convert a JSON string to an object
+---@field unserialiseJSON fun(s: string, options: table?): any|nil, string Convert a JSON string to an object
